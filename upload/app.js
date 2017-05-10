6ab834f06a54ac4d88f53f408d384fa8e7c43842
studyhbsa <studya@chacuo.net>
2017-05-10 13:54:24 +0800

    添加 file_upload 路由

--- upload/app.js
+++ upload/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var file_upload = require('./routes/file_upload');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/file_upload', file_upload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
