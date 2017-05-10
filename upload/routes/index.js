c915f59489fe348e8c36c10d2d05ce7e97324950
studyhbsa <studya@chacuo.net>
2017-05-10 11:35:40 +0800

    html信息修改为调用标题信息

--- upload/routes/index.js
+++ upload/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var getHtmls = require('../libs/buildIndexData').getHtmlTitleFromPbulic
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express',htmls:getHtmls() });
 });
 
 module.exports = router;
